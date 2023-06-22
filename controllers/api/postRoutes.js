4901deac712be92ba1616690550141d7d636f6d1
Matthew Bull <mattbull33@gmail.com>
2023-06-22 11:26:48 -0400

    seeds file and post routes didnt save

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -37,6 +37,26 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
--- seeds/seed.js
+++ seeds/seed.js
@@ -19,6 +19,12 @@ const seedDatabase = async () => {
     });
   }
 
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+    });
+  }
+  
   process.exit(0);
 };
 
